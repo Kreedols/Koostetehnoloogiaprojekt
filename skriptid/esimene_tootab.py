a20a74c92c628d36459f3a7a8b085087bf814373
Kreedols <kermo.lukas@khk.ee>
2017-03-13 20:51:45 +0200

    rohkem IF funk.

--- skriptid/esimene_tootab.py
+++ skriptid/esimene_tootab.py
@@ -18,11 +18,19 @@ while True:
 			works = True
 		except:
 			works = False
-		if works and maxsigne_cur - maxsigne > 4000:
+		if works and maxsigne_cur - maxsigne > 7000:
 			print maxsigne_cur
-			if maxsigne_cur > 15000:
-				os.system("aplay rahune_maha.mp3")
-			else:
-				os.system("aplay test.mp3")
+			if maxsigne_cur > 30000:
+				os.system("aplay 1.mp3")
+			elif: maxsigne_cur > 25000:
+				os.system("aplay 2.mp3")
+			elif: maxsigne_cur > 20000:
+				os.system("aplay 3.mp3")
+			elif: maxsigne_cur > 15000:
+				os.system("aplay 4.mp3")
+			elif: maxsigne_cur > 10000:
+				os.system("aplay 5.mp3")
+			else
+				os.system("aplay 6.mp3")
 		maxsigne = maxsigne_cur
 	time.sleep(0.01)
